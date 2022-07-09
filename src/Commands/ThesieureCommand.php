77b6c1888f3fad0dda7e2a7b096a942c500e46c0
dinhdjj <dinhdjj@gmail.com>
2022-07-09 09:16:07 +0700

    chore: configure

--- src/Commands/ThesieureCommand.php
+++ src/Commands/ThesieureCommand.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton\Commands;
+namespace Dinhdjj\Thesieure\Commands;
 
 use Illuminate\Console\Command;
 
-class SkeletonCommand extends Command
+class ThesieureCommand extends Command
 {
-    public $signature = 'skeleton';
+    public $signature = 'thesieure';
 
     public $description = 'My command';
 
--- src/ThesieureServiceProvider.php
+++ src/ThesieureServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace VendorName\Skeleton;
+namespace Dinhdjj\Thesieure;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use VendorName\Skeleton\Commands\SkeletonCommand;
+use Dinhdjj\Thesieure\Commands\ThesieureCommand;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class ThesieureServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
@@ -16,10 +16,11 @@ class SkeletonServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package
-            ->name('skeleton')
+            ->name('thesieure')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_skeleton_table')
-            ->hasCommand(SkeletonCommand::class);
+            // ->hasViews()
+            // ->hasMigration('create_thesieure_table')
+            // ->hasCommand(ThesieureCommand::class)
+        ;
     }
 }
